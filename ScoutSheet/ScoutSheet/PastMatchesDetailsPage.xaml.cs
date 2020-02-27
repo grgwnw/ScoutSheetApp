8569228134d30cb99025139ff42fe8d3e21f9861
grgwnw <grgwnw@gmail.com>
2020-02-27 16:15:58 -0500

    Update PastMatchesDetailsPage.xaml.cs

--- ScoutSheet/ScoutSheet/PastMatchesDetailsPage.xaml.cs
+++ ScoutSheet/ScoutSheet/PastMatchesDetailsPage.xaml.cs
@@ -152,7 +152,8 @@ namespace ScoutSheet
 			await Share.RequestAsync(new ShareFileRequest
 			{
 				Title = Title,
-				File = new ShareFile(Path.Combine(App.folderPathSave, "Test.csv"))
+				File = new ShareFile(Path.Combine(App.folderPathSave, "Test.csv")),
+				PresentationSourceBounds = DeviceInfo.Platform == DevicePlatform.iOS && DeviceInfo.Idiom == DeviceIdiom.Tablet? new System.Drawing.Rectangle(0, 20, 50, 40): System.Drawing.Rectangle.Empty
 			});
 		}
 	}
